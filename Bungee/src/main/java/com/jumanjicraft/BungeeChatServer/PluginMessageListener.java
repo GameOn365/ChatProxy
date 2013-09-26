ff7c2d9c5415038ff3b9368843a21bb2b14f5bc9
samoatesgames <sam@samoatesgames.com>
2013-09-26 17:51:39 +0100

    Tidied up the forwarding of chat messages.

--- Bungee/src/main/java/com/jumanjicraft/BungeeChatServer/PluginMessageListener.java
+++ Bungee/src/main/java/com/jumanjicraft/BungeeChatServer/PluginMessageListener.java
@@ -2,6 +2,8 @@ package com.jumanjicraft.BungeeChatServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Collection;
+
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -22,11 +24,16 @@ public class PluginMessageListener implements Listener {
 		}
 
 		InetSocketAddress addr = event.getSender().getAddress();
-		for (ServerInfo server : this.plugin.getProxy().getServers().values()) {
-			if (!server.getAddress().equals(addr)) {
-				if (server.getPlayers().size() != 0) {
-					server.sendData("BungeeChat", event.getData());
-				}
+		Collection<ServerInfo> servers = this.plugin.getProxy().getServers().values();
+		
+		for (ServerInfo server : servers) {
+			// Don't send the message back to the server that
+			if (server.getAddress().equals(addr))
+				continue;
+			
+			// Only send the message to a server if there are players on it
+			if (server.getPlayers().size() != 0) {
+				server.sendData("BungeeChat", event.getData());
 			}
 		}
 	}
